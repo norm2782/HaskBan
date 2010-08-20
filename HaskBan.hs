9f593e3f6fe7f8fd32d3eba29ab50c1e6d52f4e9
Roman Gonzalez <romanandreg@gmail.com>
2010-08-20 17:30:07 +0200

    Updating the types for the HaskBan.hs, also adding stubs for the SokoParser

--- HaskBan.hs
+++ HaskBan.hs
@@ -1,11 +1,29 @@
-data EnvObj = Wall
-            | Box
-            | Path
-            | Target
+module HaskBan where
+  
+  import SokoParser (parseSokoMap)
+  import qualified Data.Map as M
 
-data Surrounding = Left EnvObj
-                 | Right EnvObj
-                 | Up EnvObj
-                 | Down EnvObj
+  data CellType = Wall
+                | Box
+                | Path
+                | Target
+                deriving (Show, Eq, Ord)
+
+  data Surrounding = Left CellType
+                   | Right CellType
+                   | Up CellType
+                   | Down CellType
+                   deriving (Show, Eq, Ord)
+  
+  type Point = (Int, Int)
+
+  type SokoMap = M.Map Point CellType
+
+  data SokobanState = SokobanState {
+    player :: Point,
+    boxes :: [Point],
+    targets :: [Point],
+    cellMap :: SokoMap
+  }
 
 
